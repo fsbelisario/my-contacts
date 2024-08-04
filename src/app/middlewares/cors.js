257fa7a61747f498998dd898d6a4291d07e950ce
Felipe Belisário <fbelisario@gmail.com>
2024-08-04 16:02:06 -0300

    liberando múltiplas origens no CORS

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,24 +1,40 @@
 module.exports = (request, response, next) => {
-  // Liberado para qualquer origem de requisição
-  // response.setHeader('Access-Control-Allow-Origin', '*');
+  const allowedOrigins  =[
+    'http://localhost:3000',
+    'http://localhost:3002'
+  ];
 
-  // Liberado para qualquer origem específica
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  const { origin } = request.headers;
+  // const origin = request.header('origin'); // Forma alternativa
+  // const origin = request.get('origin'); // Forma alternativa
+  
+  const isAllowed = allowedOrigins.includes(origin);
 
-  // Liberado para qualquer método
-  response.setHeader('Access-Control-Allow-Methods', '*');
-
-  // Liberado para métodos específicos
-  // response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-
-  // Liberado para qualquer header
-  response.setHeader('Access-Control-Allow-Headers', '*');
-
-  // Liberado para headers específicos
-  // response.setHeader('Access-Control-Allow-Headers', 'x-APP-id');
-
-  // Armazena as requisições de pre-flight por 7200 segundos (cada navegador tem um limite)
-  response.setHeader('Access-Control-Max-Age', '7200');
-
+  if (isAllowed) {
+    // Liberado para qualquer origem de requisição
+    // response.setHeader('Access-Control-Allow-Origin', '*');
+  
+    // Liberado para qualquer origem específica
+    // response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  
+    // Liberado para várias origens específicas
+    response.setHeader('Access-Control-Allow-Origin', origin);
+  
+    // Liberado para qualquer método
+    response.setHeader('Access-Control-Allow-Methods', '*');
+  
+    // Liberado para métodos específicos
+    // response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  
+    // Liberado para qualquer header
+    response.setHeader('Access-Control-Allow-Headers', '*');
+  
+    // Liberado para headers específicos
+    // response.setHeader('Access-Control-Allow-Headers', 'x-APP-id');
+  
+    // Armazena as requisições de pre-flight por 7200 segundos (cada navegador tem um limite)
+    response.setHeader('Access-Control-Max-Age', '10');
+  }
+  
   next();
 };
